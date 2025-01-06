7774ba794d935fbf1e6c492ff2534e731ddacd73
SanjivaniYadav <yadavsanjivani99@gmail.com>
2025-01-06 15:44:22 +0530

    Router Added

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
+import "./index.css";
+import App from "./App.jsx";
+import CreateTrip from "./create-trip";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = document.getElementById("root");
+
+ReactDOM.createRoot(root).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/create-trip" element={<CreateTrip />} />
+    </Routes>
+  </BrowserRouter>
+);
