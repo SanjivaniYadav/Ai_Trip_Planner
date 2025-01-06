12716e09bfbee1fecbf557a83d25861a86c45b35
SanjivaniYadav <yadavsanjivani99@gmail.com>
2025-01-06 15:30:44 +0530

    Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h2>Welcome</h2>
+      <Button>submit</Button>
+    </>
+  );
+}
+
+export default App;
